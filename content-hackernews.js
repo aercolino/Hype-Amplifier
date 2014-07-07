eddb03159c1a1aca11e577afb8ef8b340ac1a5a3
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2014-07-07 14:46:46 +0300

    fixed a long standing issue that made the news shift to the right after having seen them align to the left (run_at document_start is coupled with $(function) idiom and the result is what I had expected two years ago!! -- wonderful, at last)

--- content-hackernews.js
+++ content-hackernews.js
@@ -4,31 +4,33 @@
 
 chrome.extension.sendRequest({getLocalStorage: "points_weight"}, function(response) {
 
-    var titles$ = $('td:nth-child(3).title');
-    if (! titles$.length) return;
+    $(function(){
+        var titles$ = $('td:nth-child(3).title');
+        if (! titles$.length) return;
 
-    // points and comments elements exist only if there are more than 0 points and comments respectively
-    // this means it's not possible to .find() all points and comments at once (like in the Reddit case)
-    var points   = [];
-    var comments = [];
-    $('td.subtext').each(function(k, v) {
-        points  .push(extractNumbers($(v).find('span:first')));
-        comments.push(extractNumbers($(v).find('a:last')));
-    });
+        // points and comments elements exist only if there are more than 0 points and comments respectively
+        // this means it's not possible to .find() all points and comments at once (like in the Reddit case)
+        var points   = [];
+        var comments = [];
+        $('td.subtext').each(function(k, v) {
+            points  .push(extractNumbers($(v).find('span:first')));
+            comments.push(extractNumbers($(v).find('a:last')));
+        });
 
-    var news = {
-        titles$:  titles$,
-        points:   points,
-        comments: comments
-    };
+        var news = {
+            titles$:  titles$,
+            points:   points,
+            comments: comments
+        };
 
-    var amplitude = goldenRatio($('table:first').width());
+        var amplitude = goldenRatio($('table:first').width());
 
-    var ratio = getRatio(response && response.points_weight);
+        var ratio = getRatio(response && response.points_weight);
 
-    amplify(news, amplitude, ratio, function(element, distance) {
-        $(element).parent().next().find('td.subtext').add(element)
-            .css({'padding-left': distance});
+        amplify(news, amplitude, ratio, function(element, distance) {
+            $(element).parent().next().find('td.subtext').add(element)
+                .css({'padding-left': distance});
+        });
     });
 
 });
--- content-reddit.js
+++ content-reddit.js
@@ -3,27 +3,29 @@
  */
 
 chrome.extension.sendRequest({getLocalStorage: "points_weight"}, function(response) {
-	
-	var titles$ = $('#siteTable div.link');
-	if (! titles$.length) return;
 
-    var news = {
-        titles$:  titles$,
-        points:   extractNumbers(titles$.find('div.score.unvoted')),
-        comments: extractNumbers(titles$.find('a.comments'))
-    };
+    $(function(){
+        var titles$ = $('#siteTable div.link');
+        if (! titles$.length) return;
 
-    var amplitude = goldenRatio($('#siteTable div.entry:first').width());
+        var news = {
+            titles$:  titles$,
+            points:   extractNumbers(titles$.find('div.score.unvoted')),
+            comments: extractNumbers(titles$.find('a.comments'))
+        };
 
-    var ratio = getRatio(response && response.points_weight);
+        var amplitude = goldenRatio($('#siteTable div.entry:first').width());
 
-    amplify(news, amplitude, ratio, function(element, distance) {
-        var entry$ = $(element).find('div.entry');
-        var before$ = entry$.prevUntil('.midcol');
-        if (before$.length) {
-            entry$ = before$.eq(0);
-        }
-        entry$.css({'margin-left': distance});
+        var ratio = getRatio(response && response.points_weight);
+
+        amplify(news, amplitude, ratio, function(element, distance) {
+            var entry$ = $(element).find('div.entry');
+            var before$ = entry$.prevUntil('.midcol');
+            if (before$.length) {
+                entry$ = before$.eq(0);
+            }
+            entry$.css({'margin-left': distance});
+        });
     });
 
 });
