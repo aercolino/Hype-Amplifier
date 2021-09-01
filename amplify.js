bfe9d4c0c16867a63ca5d711759ad9fd5898648b
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-09-01 11:11:38 +0200

    Extract "Amplifier" class

--- amplify.js
+++ amplify.js
@@ -1,66 +1,62 @@
-/**
- * Hype-Amplifier, Andrea Ercolino, http://andowebsit.es/blog/noteslog.com
- */
+// Hyped News Amplifier - (C) Andrea Ercolino, http://andowebsit.es
 
-function goldenRatio(length) {
-    return 0.618 * length; //0.618 is the golden ratio
-}
+ const goldenRatio = 0.618;
 
-function getRatio(points_weight) {
-    var result = {points: (points_weight || 50) / 100};
+function getRatios(pointsRatio) {
+    var result = {points: (pointsRatio || 50) / 100};
     result.comments = 1 - result.points;
     return result;
 }
 
-function extractNumbers(items$) {
-    var result = items$.map(function(k, v) {
-        const scraped = $(v).text() || '0';
-        let floatValue = parseFloat(scraped);
-        if (/k$/i.test(scraped)) {
-            floatValue *= 1000;
-        }
-        if (/m$/i.test(scraped)) {
-            floatValue *= 1000 * 1000;
-        }
-        const intValue = Math.floor(floatValue, 10);
-        return intValue;
-    }).get();
-    return result.length == 1 ? result[0] : result;
+function parseCount(scrapedText) {
+    let floatValue = parseFloat(scrapedText);
+    if (isNaN(floatValue)) {
+        floatValue = 0;
+    }
+    if (/k$/i.test(scrapedText)) { // kilo points
+        floatValue *= 1000;
+    }
+    if (/m$/i.test(scrapedText)) { // mega points
+        floatValue *= 1000 * 1000;
+    }
+    const result = Math.floor(floatValue, 10);
+    return result;
 }
 
-function amplify(news, amplitude, ratio, moveElements) {
+export default class Amplifier {
+    constructor(pointsCountList, commentsCountList, maxAmplitude, pointsRatio) {
+        this.pointsCountList = pointsCountList;
+        this.commentsCountList = commentsCountList;
+        this.maxAmplitude = maxAmplitude * goldenRatio;
 
-    var max = {
-        points:   getMax(news.points),
-        comments: getMax(news.comments)
-    };
+        this.ratios = getRatios(pointsRatio);
+        this.pointsCountMax = Math.max(...this.pointsCountList);
+        this.commentsCountMax = Math.max(...this.commentsCountList);
+        this.factor = this.maxAmplitude / (this.pointsCountMax * this.ratios.points + this.commentsCountMax * this.ratios.comments);
+    }
 
-    max.factor = amplitude / (max.points * ratio.points + max.comments * ratio.comments);
+    amplifyItem(index, amplitude) {
+        throw new Error('Expected some implementation of this method');
+    }
 
-	news.titles$.each(function (index) {
-	    var data = {
-            points:   news.points[index],
-            comments: news.comments[index]
-        };
-	    var distance = getDistance(data);
-	    moveElements(this, distance);
-	});
-
-    //---
-
-    function getMax(items) {
-        return items.reduce(function(a, b) {
-            return a < b ? b : a
+    amplifyList() {
+        this.pointsCountList.forEach((pointsCount, index) => {
+            const commentsCount = this.comments[index];
+            var amplitude = this.getAmplitude({ pointsCount, commentsCount });
+            this.amplifyItem(index, amplitude);
         });
     }
 
-    function getDistance(data) {
+    getAmplitude({ pointsCount, commentsCount }) {
         var delta = {
-            points:   max.points   - data.points,
-            comments: max.comments - data.comments
+            points: this.pointsCountMax - pointsCount,
+            comments: this.commentsCountMax - commentsCount,
         };
-        var result = Math.floor(max.factor * (delta.points * ratio.points + delta.comments * ratio.comments));
+        var result = Math.floor(this.factor * (delta.points * this.ratios.points + delta.comments * this.ratios.comments));
         return result;
     }
 
+    static parseCount(scrapedText) {
+        return parseCount(scrapedText);
+    }
 }
