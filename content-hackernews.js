56d1a7f8b3e8ad619d0d9f3d7b78f9110112a5d8
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-09-05 12:14:23 +0200

    Update "HackerNews" side

--- content-hackernews.js
+++ content-hackernews.js
@@ -1,37 +1,68 @@
-/**
- * Hype-Amplifier, Andrea Ercolino, http://andowebsit.es/blog/noteslog.com
- */
+// Hyped News Amplifier - (C) Andrea Ercolino, http://andowebsit.es
+
+const MESSAGES_ON_THE_FIRST_PAGE = 30; // units
+
+class HackerNewsAmplifier extends Amplifier {
+    constructor(rows, pointsCountList, commentsCountList, maxAmplitude, pointsRatio) {
+        super(pointsCountList, commentsCountList, maxAmplitude, pointsRatio);
+        this.rows = rows;
+    }
+
+    amplifyItem(index, amplitude) {
+        const row = this.rows[index];
+        row.style.paddingLeft = `${amplitude}px`;
+    }
+}
+
+function pointsNodesList() {
+    return Array.from(document.querySelectorAll('.score'));
+}
+
+function commentsNodesList() {
+    return Array.from(document.querySelectorAll('.subtext :nth-child(6)'));
+}
+
+function firstMessageElement() {
+    const messages = document.querySelectorAll('td:nth-child(3).title');
+    const firstPageCompleted = messages.length >= MESSAGES_ON_THE_FIRST_PAGE;
+    if (firstPageCompleted) {
+        return messages[0];
+    }
+    return undefined;
+}
+
+function movableRowsNodesList() {
+    return Array.from(document.querySelectorAll('.votelinks~.title'));
+}
+
 
 chrome.extension.sendRequest({getLocalStorage: "points_weight"}, function(response) {
 
-    $(function(){
-        var titles$ = $('td:nth-child(3).title');
-        if (! titles$.length) return;
+    let newsWidth;
 
-        // points and comments elements exist only if there are more than 0 points and comments respectively
-        // this means it's not possible to .find() all points and comments at once (like in the Reddit case)
-        var points   = [];
-        var comments = [];
-        $('td.subtext').each(function(k, v) {
-            points  .push(extractNumbers($(v).find('span:first')));
-            comments.push(extractNumbers($(v).find('a:last')));
+    function amplification() {
+        const rows = movableRowsNodesList();
+        if (rows.length === 0) return;
+
+        const pointsCountList = Amplifier.countList(pointsNodesList());
+        const commentsCountList = Amplifier.countList(commentsNodesList());
+        const amp = new HackerNewsAmplifier(rows, pointsCountList, commentsCountList, newsWidth, response && response.points_weight);
+        amp.amplifyList();
+    }
+
+    function amplify() {
+        try {
+            amplification();
+        }
+        catch (e) {
+            console.log('ERROR', e);
+        }
+    }
+
+    Amplifier.waitForElement(firstMessageElement)
+        .then((firstMessage) => {
+            newsWidth = firstMessage.clientWidth;
+            amplify();
         });
-
-        var news = {
-            titles$:  titles$,
-            points:   points,
-            comments: comments
-        };
-
-        var amplitude = goldenRatio($('table:first').width());
-
-        var ratio = getRatio(response && response.points_weight);
-
-        amplify(news, amplitude, ratio, function(element, distance) {
-            $(element).parent().next().find('td.subtext').add(element)
-                .css({'padding-left': distance});
-        });
-    });
-
 });
 
