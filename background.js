d7fbbeb7e01647bc406db2eb7ed731b7c165ae8c
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-09-06 19:25:52 +0200

    Format code; add comment

--- background.js
+++ background.js
@@ -1,21 +1,26 @@
 // Hyped News Amplifier - (C) Andrea Ercolino, http://andowebsit.es
 
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-	if (!(request.getLocalStorage && request.getLocalStorage == 'points_weight')) {
-		sendResponse({});
-		return;
-	}
-	if (!localStorage['points_weight']) {
-		localStorage['points_weight'] = 50;
-	}
-	sendResponse({ points_weight: localStorage['points_weight'] });
+    if (!(request.getLocalStorage && request.getLocalStorage == 'points_weight')) {
+        sendResponse({});
+        return;
+    }
+    if (!localStorage['points_weight']) {
+        localStorage['points_weight'] = 50;
+    }
+    sendResponse({ points_weight: localStorage['points_weight'] });
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	switch (true) {
-		case tab.url.search('^https://news.ycombinator.com/') > -1:
-		case tab.url.search('^https://www.reddit.com/') > -1:
-			chrome.pageAction.show(tabId);
-			break;
-	}
+    switch (true) {
+        case tab.url.search('^https://news.ycombinator.com/') > -1:
+        case tab.url.search('^https://www.reddit.com/') > -1:
+            chrome.pageAction.show(tabId);
+            break;
+    }
+
+    // Tab.url (https://developer.chrome.com/docs/extensions/reference/tabs/#type-Tab)
+    // The last committed URL of the main frame of the tab. This property is only present if the
+    // extension's manifest includes the "tabs" permission and may be an empty string if the tab has
+    // not yet committed.
 });
