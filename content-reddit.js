17e4d02fd581755f441eb1ee0157d71e6374dd53
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-09-10 14:35:25 +0200

    Wait for messages list element
    + Rename lots of stuff to be more precise
    + Scope points and comments to messages list

--- content-reddit.js
+++ content-reddit.js
@@ -30,21 +30,43 @@ class RedditAmplifier extends Amplifier {
     }
 }
 
-function pointsElements() {
-    return document.querySelectorAll('button[data-click-id="upvote"][id]~div');
+function pointsElements(messagesListElement) {
+    return messagesListElement.querySelectorAll(':scope [data-click-id="upvote"][id]~div');
 }
 
-function commentsElements() {
-    return document.querySelectorAll('a[data-click-id="comments"]');
+function commentsElements(messagesListElement) {
+    return messagesListElement.querySelectorAll(':scope [data-click-id="comments"]');
 }
 
-function firstMessageElement() {
-    const messages = document.querySelectorAll('div.Post');
-    const firstPageCompleted = messages.length >= MESSAGES_ON_THE_FIRST_PAGE[currentView()];
-    if (firstPageCompleted) {
-        return messages[0];
+function waitForMessagesListElement() {
+    function element() {
+        try {
+            const isUserPage = document.location.pathname.startsWith('/user/');
+            if (isUserPage) {
+                // A user can pin posts, which appear in a separate container, that we must skip.
+                // For that reason, it's much easier to get to the list in the following way
+                return document.querySelector('#ListingSort--SortPicker').parentElement.parentElement.parentElement.nextElementSibling;
+            }
+            // As of 2021-08, this is how you go from a message to the smallest news container in the Reddit page
+            return document.querySelector('div.Post').parentElement.parentElement.parentElement;
+        }
+        catch (e) {
+            return undefined;
+        }
     }
-    return undefined;
+    return Amplifier.waitForElement(element);
+}
+
+function waitForFirstMessageElement(messagesListElement) {
+    function element() {
+        const messages = messagesListElement.children;
+        const firstPageCompleted = messages.length >= MESSAGES_ON_THE_FIRST_PAGE[currentView()];
+        if (firstPageCompleted) {
+            return messages[0];
+        }
+        return undefined;
+    }
+    return Amplifier.waitForElement(element);
 }
 
 function amplifiableElements() {
@@ -59,32 +81,32 @@ function currentView() {
 
 
 chrome.storage.local.get(['points_weight'], function (response) {
-    let newsContainer;
-    let newsWidth;
-    let observer;
-    let newsCount = 0;
-    let intervalId;
+    let messagesListElement;
+    let messagesWidth;
+    let messagesListObserver;
+    let previousMessagesCount = 0;
+    let nextCheck;
 
     function amplification() {
         const rows = amplifiableElements();
         if (rows.length === 0) return;
 
-        const pointsCountList = Amplifier.countList(pointsElements());
-        const commentsCountList = Amplifier.countList(commentsElements());
-        const amp = new RedditAmplifier(rows, pointsCountList, commentsCountList, newsWidth, response && response.points_weight);
+        const pointsCountList = Amplifier.countList(pointsElements(messagesListElement));
+        const commentsCountList = Amplifier.countList(commentsElements(messagesListElement));
+        const amp = new RedditAmplifier(rows, pointsCountList, commentsCountList, messagesWidth, response && response.points_weight);
         amp.amplifyList();
     }
 
     function amplifyIfNewsChanged() {
-        const childrenCount = newsContainer.childElementCount;
-        if (childrenCount === newsCount) {
+        const messagesCount = messagesListElement.childElementCount;
+        if (messagesCount === previousMessagesCount) {
             // As soon as we see that no news were added since the previous check, we stop checking again later
-            clearInterval(intervalId);
+            clearInterval(nextCheck);
             // and instead start observing the news container again, waiting for the infinite scroll to kick in
-            observer.observe(newsContainer, { childList: true });
+            messagesListObserver.observe(messagesListElement, { childList: true });
             return;
         }
-        newsCount = childrenCount;
+        previousMessagesCount = messagesCount;
         try {
             amplification();
         }
@@ -94,19 +116,21 @@ chrome.storage.local.get(['points_weight'], function (response) {
     }
 
     function setup() {
-        Amplifier.waitForElement(firstMessageElement)
+        waitForMessagesListElement()
+            .then((element) => {
+                messagesListElement = element;
+                return waitForFirstMessageElement(element);
+            })
             .then((firstMessage) => {
-                // As of 2021-08, this is how you go from the message to the smallest news container in the Reddit page
-                newsContainer = firstMessage.parentElement.parentElement.parentElement;
-                newsWidth = firstMessage.clientWidth;
-                observer = new MutationObserver(() => {
+                messagesWidth = firstMessage.clientWidth;
+                messagesListObserver = new MutationObserver(() => {
                     // This observer is only used to detect when the Reddit page starts loading a new block of news (infinite scrolling)
-                    observer.disconnect();
+                    messagesListObserver.disconnect();
                     // From then on, we look for newly added news, between adjacent checks, at regular intervals of time
-                    intervalId = setInterval(amplifyIfNewsChanged, WAIT_FOR_NEWS_DELAY);
+                    nextCheck = setInterval(amplifyIfNewsChanged, WAIT_FOR_NEWS_DELAY);
                 });
                 // Initially, we look for newly added news. Here a timeout would be enough, but it's easier to re-use the interval setup
-                intervalId = setInterval(amplifyIfNewsChanged, WAIT_FOR_NEWS_DELAY);
+                nextCheck = setInterval(amplifyIfNewsChanged, WAIT_FOR_NEWS_DELAY);
             });
     }
 
